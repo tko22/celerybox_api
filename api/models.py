522168012d235be7944fe95ee74fa835bf36c463
Timothy Ko <tko@spikes.com>
2017-07-14 19:41:56 +0800

    changed ItemTypes database

--- api/models.py
+++ api/models.py
@@ -20,7 +20,7 @@ class ItemType(models.Model):
     name = models.CharField(max_length=100)
     typical_price = models.DecimalField(max_digits=6, decimal_places=2)
     health_index = models.FloatField(default=0)
-
+    category = models.CharField(max_length=30)
     def __str__(self):
         return self.name
 
