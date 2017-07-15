aa47f816c101e3759081a63d2c201dc9b917d08d
Timothy Ko <tko@spikes.com>
2017-07-15 18:11:15 +0800

    added a couple api endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls import url, include
 from . import views
 urlpatterns = [
-	url(r'^users/$', views.UserList.as_view()),
-	url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
-	url(r'^suppliers/$', views.SupplierList.as_view()),
-	url(r'^suppliers/(?P<pk>[0-9]+)/$', views.SupplierDetail.as_view()),
-	url(r'^itemtypes/$', views.ItemTypeList.as_view()),
-	url(r'^itemtypes/(?P<pk>[0-9]+)/$', views.ItemTypeDetail.as_view()),
-	url(r'^onsaleitems/$', views.OnSaleItemList.as_view()),
-	url(r'^onsaleitems/(?P<pk>[0-9]+)/$', views.OnSaleItemDetail.as_view()),
+    # url(r'^users/$', views.UserList.as_view()),
+    # url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+
+    url(r'^suppliers/$', views.SupplierList.as_view()),
+    url(r'^suppliers/(?P<pk>[0-9]+)/$', views.SupplierDetail.as_view()),
+    url(r'^suppliers/(?P<longitude>[+-]?([0-9]*[.])?[0-9]+)/(?P<latitude>[+-]?([0-9]*[.])?[0-9]+)/$', views.stores_list,
+        name="store list"),
+    url(r'^suppliers/(?P<pk>[0-9]+)/onsaleitems/$',views.supplier_sale_items,name="supplier on sale stuff"),
+
+    url(r'^itemtypes/$', views.ItemTypeList.as_view()),
+    url(r'^itemtypes/(?P<pk>[0-9]+)/$', views.ItemTypeDetail.as_view()),
+
+    url(r'^onsaleitems/$', views.OnSaleItemList.as_view()),
+    url(r'^onsaleitems/(?P<pk>[0-9]+)/$', views.OnSaleItemDetail.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -4,37 +4,62 @@ from rest_framework import viewsets
 from . import serializers
 from django.contrib.auth.models import User
 from rest_framework import generics
-from . import models
+from django.http import Http404
+from .models import Supplier,ItemType,OnSaleItem
+from rest_framework.views import APIView
+from django.http import HttpResponse, JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-class UserList(generics.ListCreateAPIView):
-	queryset = User.objects.all()
-	serializer_class = serializers.UserSerializer
 
 
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-	queryset = User.objects.all()
-	serializer_class = serializers.UserSerializer
+# class UserList(generics.ListCreateAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = serializers.UserSerializer
+#
+# class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = serializers.UserSerializer
 
 class SupplierList(generics.ListCreateAPIView):
-	queryset = models.Supplier.objects.all()
-	serializer_class = serializers.SupplierSerializer
+    queryset = Supplier.objects.all()
+    serializer_class = serializers.SupplierSerializer
 
-class SupplierDetail(generics.RetrieveUpdateDestroyAPIView):
-	queryset = models.Supplier.objects.all()
-	serializer_class = serializers.SupplierSerializer
+class SupplierDetail(generics.RetrieveAPIView):
+    queryset = Supplier.objects.all()
+    serializer_class = serializers.SupplierSerializer
+
+@api_view(['GET'])
+def stores_list(request,longitude,latitude):
+    if request.method == 'GET':
+        data = {'long':longitude,'lat':latitude}
+        return Response(data)
+    def get_object(self, longitude,latitude):
+        ret = {'status':'success'}
+        try:
+            ret['long'] = longitude
+            ret['lat'] = latitude
+            return HttpResponse(latitude)
+        except Supplier.DoesNotExist:
+            raise Http404
+
+@api_view(['GET'])
+def supplier_sale_items(request,pk):
+    if request.method == 'GET':
+        return Response({'pk':pk, 'stores':{'safeway','costco'}})
 
 class ItemTypeList(generics.ListCreateAPIView):
-	queryset = models.ItemType.objects.all()
-	serializer_class = serializers.ItemTypeSerializer
+    queryset = ItemType.objects.all()
+    serializer_class = serializers.ItemTypeSerializer
 
-class ItemTypeDetail(generics.RetrieveUpdateDestroyAPIView):
-	queryset = models.ItemType.objects.all()
-	serializer_class = serializers.ItemTypeSerializer
+class ItemTypeDetail(generics.RetrieveAPIView):
+    queryset = ItemType.objects.all()
+    serializer_class = serializers.ItemTypeSerializer
 
 class OnSaleItemList(generics.ListCreateAPIView):
-	queryset = models.OnSaleItem.objects.all()
-	serializer_class = serializers.OnSaleItemSerializer
+    queryset = OnSaleItem.objects.all()
+    serializer_class = serializers.OnSaleItemSerializer
 
 class OnSaleItemDetail(generics.RetrieveUpdateDestroyAPIView):
-	queryset = models.OnSaleItem.objects.all()
-	serializer_class = serializers.OnSaleItemSerializer
+    queryset = OnSaleItem.objects.all()
+    serializer_class = serializers.OnSaleItemSerializer
